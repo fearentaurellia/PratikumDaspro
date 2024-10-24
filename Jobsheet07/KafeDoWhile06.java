a24be835a9cb2ca47d2c27839e6a93e6f355333b
unknown <fearenta@gmail.com>
2024-10-24 22:47:17 +0700

    Jobsheet07

---
+++ Jobsheet07/KafeDoWhile06.java
@@ -0,0 +1,20 @@
+//package Jobsheet07;
+
+import java.util.Scanner;
+
+public class KafeDoWhile06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int kopi, teh, roti;
+        String namaPelanggan;
+        int hargaKopi = 12000, hargaTeh = 7000, hargaRoti = 20000;
+
+        do {
+
+        } while (true);
+
+        do {
+            System.out.print("Masukkan nama pelanggan (ketik 'batal' untuk keluar ): ");
+        }
+    }
+}
